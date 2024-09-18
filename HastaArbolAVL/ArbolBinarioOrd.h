e3296510aa4ca90ab8d229945c15421a336a028a
Diego Muñoz <157607003+diegostrng@users.noreply.github.com>
2024-09-18 14:24:01 -0500

    Add files via upload

---
+++ HastaArbolAVL/ArbolBinarioOrd.h
@@ -0,0 +1,33 @@
+#ifndef __ARBOLBINARIOORD_H__
+#define __ARBOLBINARIOORD_H__
+
+#include "NodoBinario.h"
+
+template< class T >
+class ArbolBinarioOrd{
+  protected:
+    NodoBinario<T>* raiz;
+  public:
+    ArbolBinarioOrd();
+    ~ArbolBinarioOrd();
+    bool esVacio();
+    T datoRaiz();
+    int altura();
+    int altura(NodoBinario<T>* nodo);
+    int tamano();
+    int tamano(NodoBinario<T>* nodo);
+    bool insertar(T val);
+    bool eliminar(T val);
+    bool buscar(T val);
+    void preOrden();
+    void preOrden(NodoBinario<T>* nodo);
+    void inOrden();
+    void inOrden(NodoBinario<T>* nodo);
+    void posOrden();
+    void posOrden(NodoBinario<T>* nodo);
+    void nivelOrden();
+};
+
+#include "ArbolBinarioOrd.hxx"
+
+#endif
