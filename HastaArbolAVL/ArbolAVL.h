05b80fa2a7c1d4a4bfb4e94c860cab8274799f8c
Diego Muñoz <157607003+diegostrng@users.noreply.github.com>
2024-09-24 02:15:11 -0500

    Update ArbolAVL.h

--- HastaArbolAVL/ArbolAVL.h
+++ HastaArbolAVL/ArbolAVL.h
@@ -6,12 +6,22 @@
 template <class T>
 class ArbolAVL : public ArbolBinarioOrd<T> {
 public:
-    bool insertar(T val);
-    bool eliminar(T val);
+
+    //Pongo en publico para poder utilizar estos metodos que se nos piden en el taller
+
+    void insertar(T val) {
+        this->raiz = insertar(this->raiz, val);
+    }
+
+    void eliminar(T val) {
+        this->raiz = eliminar(this->raiz, val);
+    }
+
+    void buscar(T val);
 
 private:
-    NodoBinario<T>* insertarRec(NodoBinario<T>* nodo, T val);
-    NodoBinario<T>* eliminarRec(NodoBinario<T>* nodo, T val);
+    NodoBinario<T>* insertar(NodoBinario<T>* nodo, T val);
+    NodoBinario<T>* eliminar(NodoBinario<T>* nodo, T val);
     NodoBinario<T>* minValueNode(NodoBinario<T>* nodo);
     NodoBinario<T>* maxValueNode(NodoBinario<T>* nodo);
     int obtenerAltura(NodoBinario<T>* nodo);
@@ -19,6 +29,8 @@ private:
     NodoBinario<T>* rotarDerecha(NodoBinario<T>* y);
     NodoBinario<T>* rotarIzquierda(NodoBinario<T>* x);
     NodoBinario<T>* balancear(NodoBinario<T>* nodo);
+    bool buscarRec(NodoBinario<T>* nodo, T val);
+    
 };
 
 #include "ArbolAVL.hxx"
